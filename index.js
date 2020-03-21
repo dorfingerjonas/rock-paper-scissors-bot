0e619dcd74a33b9f8713d4c74f9451d748aba06d
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-03-21 18:59:51 +0100

    add help command handling

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ client.on('message', (msg) => {
         if (`${players.rival}&${players.author}`.includes(msg.author.id)) {
             stopGame(msg.channel, `<@${msg.author.id}> stopped the game`);
         }
+    } else if (msg.content === '!rps-help' && !msg.author.bot) {
+        const message = 'Rock-Paper-Scissors Help-Page\n'
+        + '------------------------------------\n'
+        + '- challenge a other player with the command ``!rps-start @USERNAME``\n'
+        + '- You are interested in the rules of this game? Just type in ``!rps-help``\n'
+        + '- Do you want to stop the game? To achieve this, just type in ``!rps-stop``\n'
+        + '- If you have technical issues, please write me an email (jonas.dorfinger@gmx.at)\n'
+        + '- You want your own Bot or website? - No problem, visit my website: https://dorfingerjonas.at and use'
+        + ' the contact form or write me an email directly.\n';
+
+        msg.channel.send(message);
