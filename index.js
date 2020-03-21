f527824f563787cf9d6666b8c86ddb8274873113
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-03-21 18:59:14 +0100

    add start command handling

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ client.on('ready', () => {
     console.log('Bot started...');
 });
 
-client.login(token);
+client.on('message', (msg) => {
+    if (msg.content.includes('!rps-start') && !msg.author.bot) {
+        if (msg.content.includes('@')) {
+            players.rival = getRivalId(msg);
+            players.author = msg.author.id;
+
+            msg.channel.send(`Your rival is <@${players.rival}>`).then(() => {
+                setTimeout(() => {
+                    sendStartMessageAndContinueGame(msg.channel, players.rival);
+                }, 500);
+            });
+        } else {
+            msg.channel.send('Please mention your rival, e.g. ``!rps-start @ghopper``');
+        }
